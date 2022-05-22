5e71cf17f805a67738866babc9a128489c10502e
Kristina Chia-Chi Hsu <kristinahsu@gmail.com>
2022-05-21 22:17:02 -0700

    update data tab

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function openPage(pageName,elmnt,color) {
+function openPage(pageName,elmnt) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -9,7 +9,6 @@ function openPage(pageName,elmnt,color) {
       tablinks[i].style.backgroundColor = "";
     }
     document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
   }
   
   // Get the element with id="defaultOpen" and click on it
